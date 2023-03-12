bcf864fc4a6a2aa6fe31c6de03cce4c51416d476
anngogm98 <anngogm98@gmail.com>
2023-03-12 16:35:00 +0700

    features: files

--- src/main/java/com/dado/quanlytailieu/model/Image.java
+++ src/main/java/com/dado/quanlytailieu/model/Image.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 public class Image {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     String name;
